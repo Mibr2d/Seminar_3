f40a7d17b98a275c5bdcb7bd0b4770eb095688dd
mibr2d <hodakov7419@gmail.com>
2023-01-23 16:20:43 +0300

    first commit

---
+++ Task001/Program.cs
@@ -0,0 +1,49 @@
+﻿// 1. Напишите программу, которая принимает на вход 
+// координаты точки (X и Y), причём X ≠ 0 или Y ≠ 0 и 
+// выдаёт номер четверти плоскости, в которой находится 
+// эта точка.
+
+//Ввод целого значения
+int Promt(string message)
+{
+    System.Console.WriteLine($"{message} > ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+bool ValidateCoords(int xCoord, int yCoord)
+{
+    if (!(xCoord == 0 || yCoord == 0))
+    {
+        return true;
+    }
+    else
+    {
+        System.Console.WriteLine("Точка лежит хотя бы на одной из осей");
+        return false;
+    }
+}
+int GetQuorter(int xCoord, int yCoord)
+{
+    if (xCoord > 0 && yCoord > 0)
+    {
+        return 1;
+    }
+    if (xCoord > 0 && yCoord < 0)
+    {
+        return 4;
+    }
+    if (xCoord < 0 && yCoord > 0)
+    {
+        return 2;
+    }
+    return 3;
+
+}
+
+int x = Promt("Введите X");
+int y = Promt("Введите Y");
+if (ValidateCoords(x, y))
+{
+    int quorter = GetQuorter(x, y);
+    System.Console.WriteLine($"Номер четверти {quorter} ");
+}
